906f744601f45f8a412eb0ae3ce3ea59a2bacdcc
Nitin Thakur <nitinthakur6121@gmail.com>
2026-09-25 00:43:40 +0530

    Prepare backend for cloud deployment

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
 
